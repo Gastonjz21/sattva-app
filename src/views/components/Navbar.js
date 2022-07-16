6f7d785f19ed87bd913cf7f0edb041707780a6c5
gastonjz21 <gastonjzabala@gmail.com>
2022-07-16 14:27:52 -0300

    Ajustes de estilo

---
+++ src/views/components/Navbar.js
@@ -0,0 +1,16 @@
+import './styleComponents.css'
+
+function Navbar(){
+    return(
+        <nav className="App-Navbar">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
